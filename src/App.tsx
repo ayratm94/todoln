ef752c97a0839e8be9bc5c303c7215c03534712a
Shchukin <49319535+Fremays@users.noreply.github.com>
2022-12-06 13:14:45 +0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import './App.css'
+import {Box, List, ThemeIcon} from "@mantine/core";
+import useSWR from "swr";
+import AddTodo from "./components/AddTodo";
+import {CheckCircleFillIcon} from "@primer/octicons-react";
+
+export interface Todo {
+    id: number;
+    title: string;
+    body: string;
+    done: boolean;
+}
+
+export const ENDPOINT = "http://127.0.0.1:5173"
+
+const fetcher = (url: string) =>
+    fetch(`${ENDPOINT}/${url}`).then((r) => r.json())
+
+function App() {
+
+    const {data, mutate} = useSWR<Todo[]>("api/todos", fetcher)
+
+    async function markTodoAdDone(id: number) {
+        const updated = await fetch(`${ENDPOINT}/api/todos/${id}/done`, {
+            method: "PATCH"
+        }).then((r) => r.json());
+
+        mutate(updated)
+    }
+
+    return (
+        <Box
+            sx={(theme) => ({
+                padding: "2rem",
+                width: "100%",
+                maxWidth: "40rem",
+                margin: "0 auto"
+            })}
+        >
+            <List spacing="xs" size="sm" mb={12} center>
+                {data?.map((todo) => {
+                    return <List.Item
+                        onClick={()=> markTodoAdDone(todo.id)}
+                        key={`todo_list__${todo.id}`}
+                        icon={
+                            todo.done ? (<ThemeIcon color="teal" size={24} radius="xl">
+                                    <CheckCircleFillIcon size={20}/>
+                                </ThemeIcon>
+                            ) : (
+                                <ThemeIcon color="gray" size={24} radius="xl">
+                                    <CheckCircleFillIcon size={20}/>
+                                </ThemeIcon>)
+                        }>{todo.title}
+                    </List.Item>
+                })}
+            </List>
+            {/*{JSON.stringify(data)}*/}
+
+            <AddTodo mutate={mutate}/>
+        </Box>
+    )
+}
+
+export default App
